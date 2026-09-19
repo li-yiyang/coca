363512d553b2b9f244f005f17f3e4dfe1b658694
凉凉 <thebigbigwordl@qq.com>
2026-09-19 23:26:46 +0800

    fix: menu should be removed from parent before adding (in add-child)

--- appkit/menu.lisp
+++ appkit/menu.lisp
@@ -47,13 +47,13 @@
    :init-in-main-p t
    :title          (alx:required-argument :title)))
 
-(defmethod add-child ((container menu-element) (item menu-element))
+(defmethod add-item ((container menu-element) (item menu-element))
   (and (add-item container item) t))
 
-(defmethod remove-child ((container menu-element) (item menu-element))
+(defmethod remove-item ((container menu-element) (item menu-element))
   (remove-item container item))
 
-(defmethod add-item ((item menu-item) (child base-menu-item))
+(defmethod add-child ((item menu-item) (child base-menu-item))
   (with-slots (submenu) item
     ;; ensure submenu is initialized as menu
     (unless submenu
@@ -63,14 +63,14 @@
             (dispatch-main ()
               (invoke item-ptr "setSubmenu:" :object menu-ptr))))
         (setf submenu menu)))
-    (add-item submenu child)))
+    (add-child submenu child)))
 
-(defmethod remove-item ((item menu-item) (child base-menu-item))
+(defmethod remove-child ((item menu-item) (child base-menu-item))
   (with-slots (submenu) item
     (when submenu
-      (remove-item submenu child))))
+      (remove-child submenu child))))
 
-(defmethod add-item ((menu menu) (item base-menu-item))
+(defmethod add-child ((menu menu) (item base-menu-item))
   (with-ptr menu menu-ptr
     (with-ptr item item-ptr
       (dispatch-main ()
@@ -80,13 +80,13 @@
         (append (slot-value menu 'menu-items) (list item)))
   item)
 
-(defmethod add-item ((menu menu) (item menu-item))
+(defmethod add-child ((menu menu) (item menu-item))
   (call-next-method)
   (alx:when-let ((submenu (slot-value item 'submenu)))
     (setf (slot-value submenu 'supermenu) menu))
   item)
 
-(defmethod remove-item ((menu menu) (item base-menu-item))
+(defmethod remove-child ((menu menu) (item base-menu-item))
   (with-ptr menu menu-ptr
     (with-ptr item item-ptr
       (dispatch-main ()
@@ -95,7 +95,7 @@
   (setf (slot-value menu 'menu-items)
         (delete item (slot-value menu 'menu-items) :test #'eq)))
 
-(defmethod remove-item ((menu menu) (item menu-item))
+(defmethod remove-child ((menu menu) (item menu-item))
   (let ((res (call-next-method)))
     (alx:when-let ((submenu (slot-value item 'submenu)))
       (setf (slot-value submenu 'supermenu) nil))
@@ -169,22 +169,22 @@ Dev Note:
     (let* ((process (invoke (invoke "NSProcessInfo" "processInfo" :object)
                             "processName" :ns-string))
            (item    (make-instance 'process-menu-item :title process)))
-      (add-item item (make-instance
+      (add-child item (make-instance
                       'menu-item
                       :title  "About"
                       :action "orderFrontStandardAboutPanel:"))
-      (add-item item (make-instance 'menu-separator))
-      (add-item item (services-menu-item))
-      (add-item item (make-instance 'menu-separator))
-      (add-item item (make-instance
+      (add-child item (make-instance 'menu-separator))
+      (add-child item (services-menu-item))
+      (add-child item (make-instance 'menu-separator))
+      (add-child item (make-instance
                       'menu-item
                       :title  "Hide Other"
                       :action "hideOtherApplications:"))
-      (add-item item (make-instance
+      (add-child item (make-instance
                       'menu-item
                       :title  "Show All"
                       :action "unhideAllApplications:"))
-      (add-item item (make-instance
+      (add-child item (make-instance
                       'menu-item
                       :title  "Quit"
                       :action "terminate:"))
@@ -223,7 +223,7 @@ Return `t' if MENU is setted as main menu. "
     (menu (set-main-menu menu))
     (null (set-main-menu *default-main-menu*))))
 
-(defmethod add-item ((menu %main-menu) (item process-menu-item))
+(defmethod add-child ((menu %main-menu) (item process-menu-item))
   "The `process-menu-item' ITEM should always be added as
 the first `menu-item' of MENU. "
   (with-ptr menu menu-ptr
@@ -237,7 +237,7 @@ the first `menu-item' of MENU. "
         (cons item (slot-value menu 'menu-items)))
   item)
 
-(defmethod add-item ((menu %main-menu) (item menu-item))
+(defmethod add-child ((menu %main-menu) (item menu-item))
   "Normal `menu-item' should be inserted as last two item,
 before the `help-menu-item'. "
   (with-ptr menu menu-ptr
@@ -276,8 +276,8 @@ Parameters:
             (help    (help-menu-item)))
         (with-ptr menu ptr
           (dispatch-main ()
-            (add-item menu process)
-            (add-item menu help)
+            (add-child menu process)
+            (add-child menu help)
             (invoke (app) "setMainMenu:" :object ptr))))
       (apply #'change-class menu '%main-menu args)
       (when (boundp '*main-menu*)
